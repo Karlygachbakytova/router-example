07492cb7aadc77d7ed9413132b38628140f6ff1e
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-19 10:37:42 +0600

    some changes

--- src/components/Products/FullProducts/FullProducts.js
+++
@@ -1,9 +0,0 @@
-import classes from "./FullProducts.module.css";
-
-const FullProducts = ({ match }) => {
-    return ( 
-        <h1 className={classes.FullProducts}>Product #{match.params.id}</h1>
-     );
-}
- 
-export default FullProducts;
